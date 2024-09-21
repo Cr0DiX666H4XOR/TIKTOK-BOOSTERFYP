f682c65e1650d8bbe31fc2139419d3dfcd4ffbc6
Cr0DiX666 <161714583+Cr0DiX666H4XOR@users.noreply.github.com>
2024-09-21 12:35:18 +0700

    New Uploads Tiktok Viewer Booster FYP

---
+++ Tiktok-Booster/main.py
@@ -0,0 +1,20 @@
+import art
+import time
+from inquirer import Text, List, prompt
+from Bots.Zefoy import ZefoyAutomator
+
+
+print(art.LOGO_ART)
+
+questions = [
+    Text("post_url", message=art.input_style + "Enter Tiktok URL here"),
+    List("type", message=art.input_style + "What stat do you want to bot (Use arrows to choose)?",
+         choices=["followers", "hearts", "comment_hearts", "views", "shares", "favorites", "live_stream"]
+         )
+]
+
+answers = prompt(questions)
+
+botter = ZefoyAutomator(answers["post_url"], type=answers["type"])
+botter.launch()
+botter.send()
